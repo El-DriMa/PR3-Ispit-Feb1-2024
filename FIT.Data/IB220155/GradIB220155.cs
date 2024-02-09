6d639ea6e0a9e818efbd00e31acf5567cacfe6a5
Amna Hodžić <ahodzic649@outlook.com>
2024-02-09 13:11:08 +0100

    Add project files.

---
+++ FIT.Data/IB220155/GradIB220155.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIT.Data.IB220155
+{
+    [Table("GradoviIB220155")]
+    public class GradIB220155
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public bool Status { get; set; }
+        public DrzavaIB220155 Drzava { get; set; }
+
+    }
+}
