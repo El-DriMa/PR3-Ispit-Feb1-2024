7ab5f4d956ab0d833658f8615e4f93901dfc1594
Amna Hodžić <ahodzic649@outlook.com>
2024-02-10 22:22:00 +0100

    report done

--- FIT.WinForms/IB220155/frmIzvjestajIB220155.cs
+++ FIT.WinForms/IB220155/frmIzvjestajIB220155.cs
@@ -1,4 +1,5 @@
 ﻿using FIT.Data.IB220155;
+using FIT.Infrastructure;
 using Microsoft.Reporting.WinForms;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,9 @@ namespace FIT.WinForms.IB220155
 {
     public partial class frmIzvjestajIB220155 : Form
     {
+        DLWMSDbContext db = KonekcijaNaBazuIB220155.baza;
         DrzavaIB220155 drzava;
+        List<GradIB220155> listaGradova;
         public frmIzvjestajIB220155(DrzavaIB220155 drzava)
         {
             InitializeComponent();
@@ -23,7 +26,32 @@ namespace FIT.WinForms.IB220155
 
         private void frmIzvjestajIB220155_Load(object sender, EventArgs e)
         {
-            //reportViewer1.LocalReport.ReportPath = "../../rptUvjerenjeIB220155.rdlc";
+            var brGradova = db.GradoviIB220155.Count(x => x.Drzava == drzava);
+            var rpc = new ReportParameterCollection();
+            var rp = new ReportParameter("pBrGradova", brGradova.ToString());
+            rpc.Add(rp);
+
+            listaGradova = db.GradoviIB220155.Where(x => x.Drzava == drzava).ToList();
+
+            var tabela = new List<object>();
+            for(int i = 0; i < brGradova; i++)
+            {
+                tabela.Add(new
+                {
+                    Rb=(i+1).ToString(),
+                    Grad = listaGradova[i].ToString(),
+                    Drzava=drzava.ToString(),
+                    Aktivan = listaGradova[i].Status==true?"DA":"NE",
+                });
+            }
+
+            var rds = new ReportDataSource();
+            rds.Name = "DataSet1";
+            rds.Value = tabela;
+
+            reportViewer1.LocalReport.SetParameters(rpc);
+            reportViewer1.LocalReport.DataSources.Add(rds);
+
             this.reportViewer1.RefreshReport();
         }
     }
